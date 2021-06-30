fd122d8c8347332c31cdbdc864ff8df2fffed992
Ruslan Tsgynaok <melniqoff@gmail.com>
2021-06-30 15:46:17 +0300

    linked list

--- linkedList.go
+++ linkedList.go
@@ -2,35 +2,35 @@ package main
 
 import "fmt"
 
+type linkedList struct {
+	head   *node
+	length int
+}
+
 type node struct {
 	data int
 	next *node
 }
 
-type linkedList struct{
-	head *node
-	length int
-}
-
-func (l *linkedList) prepend(n *node) {
+func (l *linkedList) prepend(node *node) {
 	second := l.head
-	l.head = n
+	l.head = node
 	l.head.next = second
 	l.length++
 }
 
 func (l linkedList) print() {
-	toPrint := l.head
+	head := l.head
 	for l.length != 0 {
-		fmt.Println(toPrint.data)
-		toPrint = toPrint.next
+		fmt.Println(head.data)
+		head = head.next
 		l.length--
 	}
 }
 
-func (l *linkedList) delete(number int) {
+func (l *linkedList) delete(value int) {
 	currentPosition := l.head
-	for currentPosition.next.data != number {
+	for currentPosition.data != value  {
 		currentPosition = currentPosition.next
 	}
 	currentPosition.next = currentPosition.next.next
@@ -39,19 +39,16 @@ func (l *linkedList) delete(number int) {
 
 func main() {
 	list := linkedList{}
-	node1 := &node{data:48}
-	node2 := &node{data:28}
-	node3 := &node{data:18}
-	node4 := &node{data:77}
-	node5 := &node{data:102}
-	node6 := &node{data:3}
-	list.prepend(node1)
-	list.prepend(node2)
-	list.prepend(node3)
-	list.prepend(node4)
-	list.prepend(node5)
-	list.prepend(node6)
-	list.delete(18)
-	list.delete(102)
+	n1 := node{data: 48}
+	n2 := node{data: 128}
+	n3 := node{data: 783}
+	n4 := node{data: 1024}
+	n5 := node{data: 256}
+	list.prepend(&n1)
+	list.prepend(&n2)
+	list.prepend(&n3)
+	list.prepend(&n4)
+	list.prepend(&n5)
+	list.delete(128)
 	list.print()
 }
