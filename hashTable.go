fb0823297ff0236e4c0c51eafd82e2d4f5802be0
Ruslan Tsgynaok <melniqoff@gmail.com>
2021-07-14 12:12:22 +0300

    binary search and recursion

---
+++ hashTable.go
@@ -0,0 +1,18 @@
+package main
+
+const ArraySize = 100
+
+// HashTable struct
+// bucket struct
+// bucketNode struct
+
+// Insert
+// Search
+// Delete
+
+// insert
+// search
+// delete
+
+// hash
+
